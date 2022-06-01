4bb02d1670bc28baac51e14c44ea8bbefbfe9de5
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-06-01 20:38:29 +0200

    Task 1 update
    setInterval for startBtn and use getRadomColor functio to change backgroundcolor
    clearInterval for stopBtn
    + disable/ activate startBtn to be click only once during itnerval

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop');
+// getRandom color function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// click on startBtn evt
+startBtn.addEventListener('click', handleEvent);
+let timerId = null;
+function handleEvent() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+// click on stopBtn evt
+stopBtn.addEventListener('click', stopEvent);
+function stopEvent() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
