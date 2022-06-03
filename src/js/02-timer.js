1dac10bcd81fcbb7bf6e92e50f57ace680865871
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-06-03 19:33:56 +0200

    task 2 update
    set interval for remaning time for 1 sec
    remaining time renderred to interface
    added leading zero method

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,47 +9,55 @@ const timerDays = document.querySelector('[data-days');
 const timerHours = document.querySelector('[data-hours');
 const timerMinutes = document.querySelector('[data-minutes');
 const timerSeconds = document.querySelector('[data-seconds');
-
-const date = new Date();
+let ms = null;
 
 startBtn.disabled = true;
 
-// flatpickr usage
+// flatpickr library
 flatpickr(dateTimePicker, {
   enableTime: true,
   time_24hr: true,
-  defaultDate: date,
+  defaultDate: new Date(),
   minuteIncrement: 1,
+  // pops out alert when picked date is in the past
+  // enables startBtn when picked date is in the future
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() <= date.getTime()) {
+    const selectedMs = selectedDates[0].getTime();
+    if (selectedMs <= new Date().getTime()) {
       window.alert('Please choose a date in the future');
       startBtn.disabled = true;
     } else {
-      console.log(convertMs(selectedDates[0]));
       startBtn.disabled = false;
+
+      // function to calculate get remaining time & set interval
+      function convertMs(ms) {
+        setInterval(() => {
+          ms = selectedMs - new Date().getTime();
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
+
+          // Remaining days
+          const days = Math.floor(ms / day);
+          // Remaining hours
+          const hours = Math.floor((ms % day) / hour);
+          // Remaining minutes
+          const minutes = Math.floor(((ms % day) % hour) / minute);
+          // Remaining seconds
+          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+          //add leading zero
+          const addLeadingZero = value => value.toString().padStart(2, '0');
+          //pass remainig time to timer textContents
+          timerDays.textContent = addLeadingZero(days);
+          timerHours.innerHTML = addLeadingZero(hours);
+          timerMinutes.innerHTML = addLeadingZero(minutes);
+          timerSeconds.textContent = addLeadingZero(seconds);
+        }, 1000);
+      }
+      // run timer on startBtn
+      startBtn.addEventListener('click', convertMs);
     }
   },
 });
-
-// function to get remaining time
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-startBtn.addEventListener('click', passTimeToTimer);
-function passTimeToTimer() {}
