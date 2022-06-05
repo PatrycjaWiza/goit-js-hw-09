0e46654791cc883950c1020631bf0d03d6ed7341
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-06-05 18:38:11 +0200

    task 3 update initial
    -assigned constants for form elements, position initial value
    -added submit eventListener
    -function to generate promises when form submits
    -delay taken from the input value
    -position console.logs according to id count
    -amount input tells how many time console.log should be done
    -working on setting the delay for in between console.logs generation
    + needs to get delay.value in console.log

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// constants
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('button');
+const { delay, step, amount } = document.querySelector('form');
+let position = null;
+
+// submit form handleEvent
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(e) {
+  e.preventDefault();
+
+  // loop for promise generator
+  for (i = 0; i < amount.value; i++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+// function create Promise
+function createPromise(position, delay) {
+  position = i;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        setTimeout(() => {
+          resolve({ position, delay });
+        }, step.value);
+      } else {
+        // Reject
+        setTimeout(() => {
+          reject({ position, delay });
+        }, step.value);
+      }
+    }, delay.value);
+  });
+}
+
+//
+//
